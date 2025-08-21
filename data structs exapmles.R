0b57a68cf9ce100eb0aab66a447af748fbe4532b
Samruddhi Nevse <nevsesamruddhi@gmail.com>
2025-08-21 20:14:38 +0530

    Add files via upload

---
+++ data structs exapmles.R
@@ -0,0 +1,6 @@
+l1 <- list(48,"Sam",9.45)
+v <- c(1,2,3,4,5,6,7,8,9,10)
+v2 <-matrix(c(1,2,3,4,5,6,7,8,9),nrow=3,ncol=3)
+j <-factor("yes","no","yes","yes")
+d <-data.frame(rollno=c(1,2,3,4), name=c("Sam","Aaru","Ro","Niru"), items=c("pen","laptop","book","computer"))
+
